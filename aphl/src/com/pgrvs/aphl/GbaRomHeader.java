2be485fb803d3144149d22b4ee88b84e380ae522
Phillip Groves <Phillip Groves>
2017-04-25 01:12:32 -0400

    Added GNU license.

--- aphl/src/com/pgrvs/aphl/GbaRomHeader.java
+++ aphl/src/com/pgrvs/aphl/GbaRomHeader.java
@@ -1,3 +1,26 @@
+/****************************************************************************\ 
+ *                                                                           * 
+ *                     ADVANCED POKÈMON HACKING LIBRARY                      * 
+ *                                                                           * 
+ *     A Java library for helping developers modify Pokemon game data        * 
+ *                                                                           * 
+ *                Copyright (C) 2017  Phillip Groves                         * 
+ *                                                                           * 
+ * This program is free software; you can redistribute it and/or modify it   * 
+ * under the terms of the GNU General Public License as published by the     * 
+ * Free Software Foundation; either version 2 of the License, or (at your    * 
+ * option) any later version.                                                * 
+ *                                                                           * 
+ * This program is distributed in the hope that it will be useful, but       * 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT-      * 
+ * ABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the LICENSE file for    * 
+ * more details.                                                             * 
+ *                                                                           * 
+ * You should have received a copy of the GNU General Public License along   * 
+ * with this program; if not, write to the Free Software Foundation, Inc.,   * 
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.             * 
+ /****************************************************************************/
+
 package com.pgrvs.aphl;
 
 import java.nio.ByteBuffer;
